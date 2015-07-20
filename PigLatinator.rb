9a3b26c5e3d2fcf5957fa44af03230baa7d01523
Margaret Ronan <emms@Margarets-MacBook-Pro.local>
2015-07-20 17:33:33 -0400

    break out announcing methods and make unnecessary formatting changes

--- PigLatinator.rb
+++ PigLatinator.rb
@@ -1,15 +1,15 @@
 class PigLatinateable
 
 	def initialize
-		puts 	"\n
-				What is the phrase you\'re looking to PigLatinate?"
+		puts 	"What is the phrase you\'re looking to PigLatinate?".center(70)
 		@english_phrase = gets.chomp
 
 		if @english_phrase == ''
-			puts 'We can\'t really do much with an empty string!'
+			puts "We can\'t really do much with an empty string!".center(70)
 			exit
 		end
 	end
+
 
 	def piglatinate
 		pre_piglatinated_phrase = @english_phrase.to_s.split(" ")
@@ -28,18 +28,6 @@ class PigLatinateable
 
 		@piglatinated_phrase = @piglatinated_phrase[0..-2]
 
-		3.times do 
-			puts 	"\n
-					*********************\n" 
-		end
-
-		puts 	"\n
-				Eatnay!\n 
-				\'#{@piglatinated_phrase.upcase}\'\n
-				is the PigLatinated version of
-				\'#{@english_phrase}\'\n
-				...oolcay, ightray?"
-
 	end
 
 	def take_it_back_to_english
@@ -53,25 +41,73 @@ class PigLatinateable
 
 		@re_englished_phrase = @re_englished_phrase[0..-2]
 
-		3.times do 
-			puts 	"\n
-					*********************\n" 
-		end
-		puts 	"\n
-				\'#{@re_englished_phrase.upcase}\'\n
-				is the re-Englished version of\n
-				\'#{@piglatinated_phrase}\'\n
-				...which is the PigLatinated version of\n
-				\'#{@english_phrase}\'\n
-				...got it?"
+	end
 
+
+	def dramatically_announce_piglatinization
+
+		3.times do 
+			puts 	"***piglatinating in progress***".center(70) 
+			puts 	
+		end
+
+		puts 	" ----------------------------------------".center(70)
+		puts
+		puts
+		puts
+		puts	">>> \'#{@piglatinated_phrase.upcase}\' <<<".center(70)
+		puts
+		puts	"is the PigLatinated version of \'#{@english_phrase}\'".center(70)
+		puts	"...oolcay, ightray?".center(70)
+		puts
+		puts
+		puts
+		puts 	" ----------------------------------------".center(70)
+		puts
+		
+	end
+
+
+	def dramatically_announce_re_englishization
+		3.times do 
+			puts 	"***re-renglishinating in progress***".center(70)
+			puts
+		end
+
+		puts 	" ----------------------------------------".center(70)
+		puts
+		puts 
+		puts
+		puts 	">>> \'#{@re_englished_phrase.upcase}\' <<<".center(70)
+		puts
+		puts	"is the re-Englished version of \'#{@piglatinated_phrase}\',".center(70)
+		puts	"which is the PigLatinated version of \'#{@english_phrase}\'".center(70)
+		puts 	"...got it?".center(70)
+		puts
+		puts
+		puts
+		puts 	" ----------------------------------------".center(70)
+		puts
+
+	end
+
+
+	def test
+		if @re_englished_phrase.upcase == @english_phrase.upcase
+			puts "Wooo! The phrase was PigLatinated and Re-Englishinated as expected!"
+		else
+			puts "Uh oh....something isn\'t right..."
+		end
 	end
 
 end
 
 phrase = PigLatinateable.new 
 phrase.piglatinate
+phrase.dramatically_announce_piglatinization
 phrase.take_it_back_to_english
+phrase.dramatically_announce_re_englishization
+phrase.test
 
 
 
